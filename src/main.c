cfb33a7f2d217def96564a3c9429f463d1833db8
Jackson Brough <jacksontbrough@gmail.com>
2026-03-26 18:43:11 -0600

    Set up the system clock and HSI (High Speed Internal) oscillator

--- src/main.c
+++ src/main.c
@@ -11,8 +11,29 @@ void HAL_MspInit(void) {
     __HAL_RCC_PWR_CLK_ENABLE();
 }
 
+void system_clock_config(void) {
+    RCC_OscInitTypeDef oscillator = {0};
+    oscillator.OscillatorType      = RCC_OSCILLATORTYPE_HSI;
+    oscillator.HSIState            = RCC_HSI_ON;
+    oscillator.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+    oscillator.PLL.PLLState        = RCC_PLL_NONE;
+    if (HAL_RCC_OscConfig(&oscillator) != HAL_OK) {
+        while (1) {}
+    }
+
+    RCC_ClkInitTypeDef clock = {0};
+    clock.ClockType      = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1;
+    clock.SYSCLKSource   = RCC_SYSCLKSOURCE_HSI;
+    clock.AHBCLKDivider  = RCC_SYSCLK_DIV1;
+    clock.APB1CLKDivider = RCC_HCLK_DIV1;
+    if (HAL_RCC_ClockConfig(&clock, FLASH_LATENCY_0) != HAL_OK) {
+        while (1) {}
+    }
+}
+
 int main(void) {
     HAL_Init();
+    system_clock_config();
 
     while (1) {
     }
